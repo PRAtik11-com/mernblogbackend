8ea5118fd12c4fccbe9e27e9d4453d5603697336
PRAtik11-com <pratikaamrutkar9999@gmail.com>
2025-06-18 16:52:44 +0530

    vercel url added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:5174"],
+  origin: ["http://localhost:5173","http://localhost:5174","mernblogfrontend-two.vercel.app","mernblogfrontend-1ngv0nctj-pratik-amrutkars-projects.vercel.app"],
   credentials: true
 }))
 
