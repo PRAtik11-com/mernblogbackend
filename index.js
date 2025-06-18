213766a06b48a6ff97450777d5d032d33e9eca5f
PRAtik11-com <pratikaamrutkar9999@gmail.com>
2025-06-18 17:22:44 +0530

    vercel url added again

--- index.js
+++ index.js
@@ -6,26 +6,29 @@ require("dotenv").config();
 var cookieParser = require("cookie-parser");
 const postRouter = require("./routes/post.routes");
 const commentRouter = require("./routes/comment.routes");
-const path = require("path"); 
+const path = require("path");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:5174","mernblogfrontend-two.vercel.app","mernblogfrontend-1ngv0nctj-pratik-amrutkars-projects.vercel.app"],
-  credentials: true
-}))
-
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://mernblogfrontend-two.vercel.app/",
+      "https://mernblogfrontend-1ngv0nctj-pratik-amrutkars-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
-app.use("/api/comment",commentRouter)
+app.use("/api/comment", commentRouter);
 app.use("/user", express.static(path.join(__dirname, "uploads/user")));
 app.use("/post", express.static(path.join(__dirname, "uploads/post")));
-
-
 
 app.listen(process.env.PORT || 3000, async () => {
   try {
